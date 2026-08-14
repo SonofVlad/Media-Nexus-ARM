981b87e795c9dc36b75e893e14ddc2736e076c16
SonofVlad <271786831+SonofVlad@users.noreply.github.com>
2026-08-14 08:53:42 -0700

    Move managed data under Media Nexus ARM tree

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Media Nexus ARM contributors")]
 [assembly: AssemblyProduct("Media Nexus ARM")]
 [assembly: AssemblyCopyright("Copyright (c) 2026 Media Nexus ARM contributors")]
-[assembly: AssemblyVersion("0.7.13.0")]
-[assembly: AssemblyFileVersion("0.7.13.0")]
+[assembly: AssemblyVersion("0.7.14.0")]
+[assembly: AssemblyFileVersion("0.7.14.0")]
